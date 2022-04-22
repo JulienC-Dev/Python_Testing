4c76407753ee59376cc505c322b798f480243a7c
JulienC-Dev <julien.cormier@yahoo.fr>
2022-04-22 12:47:24 +0200

    modification du test pour contrôler le solde de points du club et de la compétition

--- tests/unitaires_tests/test_purchase_places_unitaires.py
+++ tests/unitaires_tests/test_purchase_places_unitaires.py
@@ -1,16 +1,21 @@
 
-
-def test_purchase_places_valid_balance_point_club(client, name, name_competion, point_club):
+def test_purchase_places_valid_balance_point_club_and_point_competition(client, name, name_competion,
+                                                                        point_club, points_competion):
     input_places_desire = "2"
     assert int(point_club['point'][0]) - int(input_places_desire) == 2
     name = name['name']
     name_competion = name_competion['name']
-    response = client.post('/purchasePlaces', data=dict(club=name, competition=name_competion, places=input_places_desire),
-                           follow_redirects=True )
+    response = client.post('/purchasePlaces', data=dict(club=name, competition=name_competion,
+                           places=input_places_desire), follow_redirects=True)
     data = response.data.decode()
     point_club = int(point_club['point'][0]) - int(input_places_desire)
     assert f'<p>Points available: {point_club}</p>' in data
     assert '<li>Great-booking complete!</li>' in data
+
+    expected_competition_points = "23"
+    assert int(points_competion['numberOfPlaces'][0]) - int(input_places_desire) == int(expected_competition_points)
+    points_competion = int(points_competion['numberOfPlaces'][0]) - int(input_places_desire)
+    assert f'Number of Places: {points_competion}' in data
 
 
 def test_purchase_place_more_than_balance(client, name, name_competion, point_club):
@@ -22,8 +27,8 @@ def test_purchase_place_more_than_balance(client, name, name_competion, point_cl
                            follow_redirects=True )
     data = response.data.decode()
     assert '<li>booking not complete! insufficient numbers of points</li>' in data
-
-
+#
+#
 def test_purchase_place_negative_input(client, name, name_competion):
     input_places_desire = "-10"
     assert int(input_places_desire) < 0
@@ -34,23 +39,6 @@ def test_purchase_place_negative_input(client, name, name_competion):
                            follow_redirects=True)
     data = response.data.decode()
     assert '<li>booking not complete! invalid numbers of points</li>' in data
-
-
-def test_purchase_place_withdraw_competition_points(client, name, name_competion, points_competion):
-    input_places_desire = "2"
-    expected_competition_points = "23"
-    assert int(points_competion['numberOfPlaces'][0]) - int(input_places_desire) == int(expected_competition_points)
-
-    # print(points_competion)
-    # name = name['name']
-    # name_competion = name_competion['name']
-    # response = client.post('/purchasePlaces',
-    #                        data=dict(club=name, competition=name_competion, places=input_places_desire),)
-    # data = response.data.decode()
-    # print(points_competion)
-    # print(data)
-    # expected_output_points_competiion = 23
-    # assert f'Number of Places: 21' in data
 
 
 def test_purchase_place_zero_input(client, name, name_competion):
