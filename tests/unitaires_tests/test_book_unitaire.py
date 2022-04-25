e33ad21069714c071f40e0f401b996fa7c106db4
JulienC-Dev <julien.cormier@yahoo.fr>
2022-04-25 15:54:39 +0200

    refactorisation du test invalid url et valid url function book

--- tests/unitaires_tests/test_book_unitaire.py
+++ tests/unitaires_tests/test_book_unitaire.py
@@ -1,19 +1,28 @@
 from flask import url_for
+from Python_Testing import server
 
 
-def test_book_valid_data_url(client, app, name, name_competion):
-    name_competion = name_competion["name"][0]
-    name = name["name"][0]
+def test_book_valid_data_url(mocker, client, app):
+    fake_club = [{"name": "Iron Temple", "email": "admin@irontemple.com", "points": "4"}]
+    fake_competitions = [{'name': 'Spring Festival', 'date': '2020-03-27 10:00:00', 'numberOfPlaces': '215'}]
+    club_valid_name = fake_club[0]['name']
+    competition_valid_name = fake_competitions[0]['name']
     with client:
         with app.app_context():
-            assert client.get(url_for('book', club=name, competition=name_competion)).status_code == 200
+            mocker.patch.object(server, 'competitions', fake_competitions)
+            mocker.patch.object(server, 'clubs', fake_club)
+            assert client.get(url_for('book', club=club_valid_name, competition=competition_valid_name)).status_code == 200
 
 
-def test_book_invalid_data_url(client, app):
+def test_book_invalid_data_url(mocker, client, app):
+    fake_club = [{"name": "Iron Temple", "email": "admin@irontemple.com", "points": "4"}]
+    fake_competitions = [{'name': 'Spring Festival', 'date': '2020-03-27 10:00:00', 'numberOfPlaces': '215'}]
     name_competion = "invalid Name competition"
     name = "invalid Name club"
     with client:
         with app.app_context():
+            mocker.patch.object(server, 'competitions', fake_competitions)
+            mocker.patch.object(server, 'clubs', fake_club)
             res = client.get(url_for('book', club=name, competition=name_competion), follow_redirects=True)
             data = res.data.decode()
             assert '<li>Something went wrong-please try again</li>' in data
