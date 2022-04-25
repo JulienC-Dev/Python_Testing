80535cd819948a5a0c6693f01e9588a6e966a799
JulienC-Dev <julien.cormier@yahoo.fr>
2022-04-25 16:12:25 +0200

    modification tests integration - test vérifie la deconnexion d'un utilisateur precdement connecterle controle dun utilisateur connecté

--- tests/integrations_tests/test_logout_integrations.py
+++ tests/integrations_tests/test_logout_integrations.py
@@ -1,4 +1,5 @@
 import flask
+from Python_Testing import server
 
 
 def test_logout_route_without_connexion_user(app):
@@ -8,10 +9,18 @@ def test_logout_route_without_connexion_user(app):
         assert flask.request.path == "/"
 
 
-def test_logout_route_connexion_user(app, email):
+def test_logout_route_connexion_user(mocker, app):
+    fake_club = [{"name": "Iron Temple", "email": "admin@irontemple.com", "points": "4"}]
+    fake_competitions = [{'name': 'Spring Festival', 'date': '2020-03-27 10:00:00', 'numberOfPlaces': '215'}]
+    valid_email = {"email": "admin@irontemple.com"}
     with app.test_client() as client:
-        response = client.post("/showSummary", data=email, follow_redirects=True)
+        mocker.patch.object(server, 'competitions', fake_competitions)
+        mocker.patch.object(server, 'clubs', fake_club)
+        response = client.post("/showSummary", data=valid_email)
+        data = response.data.decode()
         assert response.status_code == 200
+        name_db = fake_club[0]['name']
+        assert f'<h2>Welcome, {name_db} </h2>' in data
         res_logout = client.get("/logout", follow_redirects=True)
         assert res_logout.status_code == 200
         assert flask.request.path == "/"
