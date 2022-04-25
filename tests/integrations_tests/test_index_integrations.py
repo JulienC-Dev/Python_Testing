8ccbfbe97442739c4634fb9d6d02f880549c5d95
JulienC-Dev <julien.cormier@yahoo.fr>
2022-04-25 15:37:01 +0200

    modification du test d integration scenario route index vers page de l utilisateur

--- tests/integrations_tests/test_index_integrations.py
+++ tests/integrations_tests/test_index_integrations.py
@@ -1,7 +1,14 @@
 
-def test_index_route_login_page_to_welcome_page(client, email):
-    response = client.get("/")
+
+def test_index_route_login_page_to_welcome_page(mocker, client):
+    fake_club = [{"name": "Iron Temple", "email": "admin@irontemple.com", "points": "4"}]
+    valid_email = {"email": ["admin@irontemple.com"]}
+    response = client.get("/", follow_redirects=True)
     assert response.status_code == 200
-    response = client.post("/showSummary", data=email)
-    assert response.status_code == 200
+    mocker.patch.object(server, 'clubs', fake_club)
+    res = client.post("/showSummary", data=valid_email, follow_redirects=True)
+    data = res.data.decode()
+    assert res.status_code == 200
+    fake_club_name = fake_club[0]['name']
+    assert f'<h2>Welcome, {fake_club_name} </h2>' in data
 
